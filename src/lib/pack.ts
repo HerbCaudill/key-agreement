3b10988bf242ed0eb3dbc4893ed2d273df456857
HerbCaudill <herb@devresults.com>
2020-10-28 15:28:11 +0100

    lib/pack.ts: create

---
+++ src/lib/pack.ts
@@ -0,0 +1,5 @@
+﻿import { base64 } from '@herbcaudill/crypto'
+import msgpack from 'msgpack-lite'
+
+export const pack = (o: any) => base64.encode(msgpack.encode(o))
+export const unpack = (s: string) => msgpack.decode(base64.decode(s))
