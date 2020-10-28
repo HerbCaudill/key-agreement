874614a6216f2d2da63395a829647f250282187d
HerbCaudill <herb@devresults.com>
2020-10-28 15:28:17 +0100

    lib/PublicKeyLookup.ts: create

---
+++ src/lib/PublicKeyLookup.ts
@@ -0,0 +1,16 @@
+﻿// dummy PKI for testing purposes
+
+import { Base64 } from '@herbcaudill/crypto'
+import { ID, PublicKeyLookup } from '../types'
+
+export class MockPublicKeyLookup implements PublicKeyLookup {
+  private keys = new Map<ID, Base64>()
+
+  set(id: ID, publicKey: Base64) {
+    this.keys.set(id, publicKey)
+  }
+
+  get(id: ID) {
+    return this.keys.get(id)
+  }
+}
